15b6eee437e507dbd971b3ab10b14678be41c345
Helosz2 <heloisa.firmino@escolar.ifrn.edu.br>
2025-05-26 11:51:13 -0300

    aula 26/05

--- My project 2d helo/Assets/script/player.cs
+++ My project 2d helo/Assets/script/player.cs
@@ -10,23 +10,27 @@ public class Player : MonoBehaviour
         numero = 0;
     }
 
-    
+
     void Update()
     {
-       //Debug.log(numero);
-       //numero = numero + 1;
+        //Debug.log(numero);
+        //numero = numero + 1;
 
-       if (Input.GetKey(KeyCode.A))
-       {
-           gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
-       }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+        }
 
-       if (Input.GetKey(KeyCode.D))
-       {
-           gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0,0);
-       }
-           
-           
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            gameObject.transform.position = new Vector4(velocidade * Time.deltaTime, 0, 0);
+
+        }
     }
 }
 

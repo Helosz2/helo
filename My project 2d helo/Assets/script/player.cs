6c45634782fdb10d3af710fece12293b63c645e5
Helosz2 <heloisa.firmino@escolar.ifrn.edu.br>
2025-06-23 11:58:34 -0300

    23/06

--- My project 2d helo/Assets/script/player.cs
+++ My project 2d helo/Assets/script/player.cs
@@ -1,35 +1,59 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int numero;
+    
     public float velocidade = 40;
-        
+    public float forcaDoPulo = 4;
+    
+    private bool noChao = false;
+    
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    
     void Start()
     {
-        numero = 0;
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-
+       
     void Update()
     {
-        //Debug.log(numero);
-        //numero = numero + 1;
-
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
+            sprite.flipX = true;
         }
-
+        
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+            sprite.flipX = false;
         }
 
-        if (Input.GetKey(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
         {
-            gameObject.transform.position = new Vector4(velocidade * Time.deltaTime, 0, 0);
+            rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
+        }
 
+    }
+
+    void OnCollisionEnter2D(Collision2D colisao)
+    {
+        //if (colisao.gameObject.tag == "Chao")
+        if(colisao.gameObject.CompareTag("Chao"))
+        {
+            noChao = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D colisao)
+    {
+        if(colisao.gameObject.CompareTag("Chao"))
+        {
+            noChao = false;
         }
     }
 }
